2e7409dc7755dbb995246d6aeb5b7e7310b1d020
Trong Nguyen <nguyentrong101094@gmail.com>
2023-10-23 16:48:53 +0700

    instantiate tutorial display in display container instead of changing its parent after instantiating so it has correct value for rect transform.

--- Scripts/TutorialStep.cs
+++ Scripts/TutorialStep.cs
@@ -73,9 +73,8 @@ namespace Omnilatent.TutorialMaker
             if (!isDone && TutorialManager.CanShowTutorial(GetData()))
             {
                 //Tutorial Display will be instantiated as child of the TutorialStep's transform. Then it will set parent to displayContainer
-                m_TutorialDisplay = Instantiate(GetData().displayObject, transform).GetComponent<ITutorialDisplay>();
+                m_TutorialDisplay = Instantiate(GetData().displayObject, displayContainer).GetComponent<ITutorialDisplay>();
                 TutorialManager.OnShowTutorial(GetData(), m_TutorialDisplay);
-                m_TutorialDisplay.GetTransform().SetParent(displayContainer);
                 m_TutorialDisplay.Setup(GetData(), gameObject);
                 m_TutorialDisplay.onComplete += CompleteStep;
                 //isDone = false;
